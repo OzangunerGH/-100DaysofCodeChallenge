1751a01c0c5af0721b6f7a4c08f1bbaca9301fe9
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2021-12-26 21:03:23 +0300

    Update main.py

--- Day017/main.py
+++ Day017/main.py
@@ -1,6 +1,11 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+#Importing classes and question data.
+
+
+# Getting the question text and answer and creating a Question object from each pair of question text and question answers, 
+# then passing those question objects in question_bank list variable.
 
 question_bank = []
 for question in question_data:
@@ -9,7 +14,7 @@ for question in question_data:
     question = Question(question_text, question_answer)
     question_bank.append(question)
 
-
+#Creating the quiz object from QuizBrain class and using necessary methods to start the game.
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
