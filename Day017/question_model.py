7c5439266f94d9133bcfbb91d52907d632048228
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2021-12-26 21:00:40 +0300

    Create question_model.py
    Defining Question class to store question text and answer.

---
+++ Day017/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+  """This is a class named Question, to store the question text and correct answer of the question."""
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
