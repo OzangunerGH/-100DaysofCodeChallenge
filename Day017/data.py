0951f823c81f005b3159ae0f7e52095623fbe8c6
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2021-12-26 20:57:35 +0300

    Create data.py
    List of questions to be used in Quiz Game.

---
+++ Day017/data.py
@@ -0,0 +1,83 @@
+question_data = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "French is an official language in Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Sonic the Hedgehog universe, Tails&#039; real name is Miles Prower.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Ana was added as a new hero for the game Overwatch on July 19th, 2016.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "According to Greek Mythology, Atlas was an Olympian God.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, "
+                    "and artists who died at the age of 27.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A scalene triangle has two sides of equal length.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Slovakia is a member of European Union-",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
