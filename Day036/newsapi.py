6496d4c3203b3e0ed0b02ad56a64a9702c1bc538
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-01-19 19:43:05 +0300

    Create newsapi.py
    This block of code gets the 3 most recent article headers and their URL's of the company in question( TESLA) and put the information in a message_text variable.
    
    Later on this message_text variable is used as the body of SMS, so the user can check articles to see what is causing the percentage change in the stock.

---
+++ Day036/newsapi.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import date
+
+# This block of code gets the last 3 news articles related to the company defined at COMPANY_NAME variable.
+# The content of the news articles are their headers and URLs to read the full article.
+
+API_KEY = "c298e4d172334929a54796515957e12a"
+COMPANY_NAME = "Tesla Inc."
+DATE = str(date.today())
+SORT_TYPE = "publishedAt"
+parameters = {"q": COMPANY_NAME,
+              "from": DATE,
+              "sortBy": SORT_TYPE,
+              "apiKey": API_KEY}
+
+response = requests.get("https://newsapi.org/v2/everything", params=parameters)
+response.raise_for_status()
+data = response.json()["articles"]
+message_text = ""
+for i in range(3):
+    message = data[i]["title"]
+    url = data[i]["url"]
+    message_text += f"{message},\n {url}\n"
