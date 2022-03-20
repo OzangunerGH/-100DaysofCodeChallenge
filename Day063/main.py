84546600b6c361898306b805b7f47cdac6eef06d
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-03-20 13:28:16 +0300

    Add files via upload
    Day063 of #100DaystoCode Challenge. This is a Book rating website where you can add new books, add ratings, delete them, or edit their rankings as desired. The data is saved to an SQL Database (Built by Flask_WTF - SQLAlchemy), and updated / deleted from the database when the user initiates an update or delete operation.

---
+++ Day063/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+all_books = []
+db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        new_book = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+
+@app.route('/edit', methods=['POST', 'GET'])
+def edit():
+    if request.method == 'POST':
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template('edit.html', book=book_selected)
+
+
+@app.route('/delete')
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run()
