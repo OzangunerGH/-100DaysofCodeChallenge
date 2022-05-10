2f437f34d18300374fe9f1e7a16a235f1870f0be
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-05-10 13:39:46 +0300

    Add files via upload
    Day086 of #100DaystoCode Challenge. This is a breakout game written with Turtle library.

---
+++ Day086/ball.py
@@ -0,0 +1,30 @@
+import random
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.fillcolor("white")
+        self.color("white")
+        self.speed("slow")
+        self.penup()
+        self.xmove = 10
+        self.ymove = 10
+
+    def move(self):
+        new_x = self.xcor () + self.xmove
+        new_y = self.ycor() + self.ymove
+        self.goto(new_x,new_y)
+
+
+    def paddle_bounce(self):
+        self.xmove *= -1
+
+
+    def wall_bounce(self):
+        self.ymove *= -1
+
+
+
+
---
+++ Day086/test.py
@@ -0,0 +1,4 @@
+import random
+var_a = random.uniform(-1,0)
+
+print(var_a)
